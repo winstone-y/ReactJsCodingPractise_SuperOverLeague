a7cb26cd9988afcbf8e6e4c96caeeffe6ae96fb9
Your Name <you@example.com>
2022-12-05 07:25:47 +0000

    Test cases passed but showing empty page in preview CCBP IDE

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  
+    <h1 class="heading">Super Over</h1>
+    <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"/>
+    <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png"/>
+  
+);
+
+ReactDOM.render(element, document.getElementById("root"));
+
+
